1afe6612573c4ada0eda5b66113f867afee16841
Kotocry <74236321+Kotocry@users.noreply.github.com>
2021-09-20 10:03:16 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//Wyœwietlenie tekstu
+  cout << "Hello World! \n"; //"\n" oznacza enter
+  cout << "Bruh";
+  int myNum = 5;  
+  double myFloatNum = 5.99;
+  char myLetter = 'D';
+  string myText = "Hello";
+  bool myBoolean = true;
+  return 0;
+} 
